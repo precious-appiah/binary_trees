8051ea5dba5a2de5995e1347309232a6ef5939cc
Francis Thore <thorefrancis@gmail.com>
2024-01-31 18:54:14 +0200

    fix: when parent->right not null

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,6 +23,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
+	if (parent->right)
+		new_node->right = parent->right;
 	parent->right = new_node;
 
 	return (new_node);
