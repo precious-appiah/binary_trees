6a592b3956e045d4a036d357de4ef08388b51fb0
precious-appiah <appiahprecious99@gmail.co>
2024-01-31 18:21:12 +0000

    task 4 betty fix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_leaf- function to check leaf node 
+* binary_tree_is_leaf- function to check leaf node
 * @node: node to check
 * Return: integer
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node->left == NULL && node->right == NULL)
-        return (1);
-    return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
 }
