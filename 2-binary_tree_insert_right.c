15e3b3da08b3b1bf861f8eaa4dc51a79ee400755
Francis Thore <thorefrancis@gmail.com>
2024-02-02 01:18:38 +0200

    betty task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -28,8 +28,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		new_node->right = parent->right;
 		new_node->right->parent = new_node;
 	}
-		
 	parent->right = new_node;
-
 	return (new_node);
 }
